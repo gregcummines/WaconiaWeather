701e978750140546ddbc6396a990abdf1f88c985
Greg Cummines <greg_cummines@starkey.com>
2018-12-28 08:18:44 -0600

    first commit

---
+++ WaconiaWeather/WaconiaWeather/ViewModels/MainViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WaconiaWeather.ViewModels
+{
+    internal class MainViewModel : ViewModelBase
+    {
+        public MainViewModel()
+        {
+        }
+
+        private string currentTemperature = "Unknown";
+        public string CurrentTemperature
+        {
+            get { return currentTemperature; }
+            set { SetProperty(ref currentTemperature, value);  }
+        }
+    }
+}
